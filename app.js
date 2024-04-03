4920d2b21f6335bb8c8bfd20aeac2324ac8c7afb
MeawWWWW <kamonnuth.lee@student.mahidol.edu>
2024-04-03 15:42:32 +0700

    create gitignore file

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-print("dress")
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => res.send('Hello World Here are the update version'))
+
+app.listen(3000, () => console.log('server start'))
